ec4dad2bf7cd3f2aa0a2abc26a1b1824851e68b1
reyes-ale <147879016+reyes-ale@users.noreply.github.com>
2024-10-02 17:05:19 -0600

    Add files via upload

---
+++ Recursivas_AlejandraReyes/src/main/java/com/mycompany/recursivas_alejandrareyes/Recursivas_AlejandraReyes.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.recursivas_alejandrareyes;
+
+/**
+ *
+ * @author aleja
+ */
+public class Recursivas_AlejandraReyes {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
